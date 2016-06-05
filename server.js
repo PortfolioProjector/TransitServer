bcbf492c2296815a95a39b555fa2bc5959f6197e
Gibryon Bhojraj <gibryonbhojraj@gmail.com>
2016-06-05 00:17:47 +0000

    Removed unnecessary landing page.

--- server.js
+++ server.js
@@ -4,11 +4,7 @@ var express = require("express");
 var app = express();
    
 app.use('/public', express.static(process.cwd() + '/app/public'));
-   
-app.get('/', function(req, res){
-    res.sendFile(process.cwd() + '/app/public/index.html');
-})
-    
+
 var server = app.listen(process.env.PORT, function(){
     console.log("Server is listening on " + process.env.PORT);
 });
