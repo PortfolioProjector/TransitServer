cdf16b803765cc69d7c1e4f7a71d992f7a3134b6
Gibryon Bhojraj <gibryonbhojraj@gmail.com>
2016-06-05 00:11:02 +0000

    Server works to send data to Intel XDK.

---
+++ server.js
@@ -0,0 +1,23 @@
+'use strict';
+
+var express = require("express");
+var app = express();
+   
+app.use('/public', express.static(process.cwd() + '/app/public'));
+   
+app.get('/', function(req, res){
+    res.sendFile(process.cwd() + '/app/public/index.html');
+})
+    
+var server = app.listen(process.env.PORT, function(){
+    console.log("Server is listening on " + process.env.PORT);
+});
+
+var io = require('socket.io')(server);
+
+io.on('connection', function (socket) {
+      socket.emit('news', { hello: 'world' });
+      socket.on('my other event', function (data) {
+        console.log(data);
+      });
+});
